3f2aa689f3c05fa66628600844c4745a653abbd4
LucaPacioselli <lucapacioselli@gmail.com>
2025-04-29 09:58:52 +0200

    processes true

--- workshop2025_demo/RDataFrame/VBS/preselection_UL-AF30.py
+++ workshop2025_demo/RDataFrame/VBS/preselection_UL-AF30.py
@@ -173,7 +173,7 @@ if __name__ == '__main__':
 
     if distributed == True:
         RDataFrame = ROOT.RDF.Experimental.Distributed.Dask.RDataFrame
-        client = LocalCluster()).get_client()
+        client = LocalCluster().get_client()
         #client = Client(cluster) #address="tcp://127.0.0.1:"+str(sched_port))
         #client.restart()
         try:
