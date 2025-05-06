482005da67b0db3bba50f8fe539d9a315ce2fa82
LucaPacioselli <lucapacioselli@gmail.com>
2025-05-06 15:23:45 +0200

    proxy handling

--- workshop2025_demo/RDataFrame/VBS/preselection_UL-AF30.py
+++ workshop2025_demo/RDataFrame/VBS/preselection_UL-AF30.py
@@ -35,7 +35,7 @@ if __name__ == '__main__':
         # from urllib import request
         # request.urlretrieve("https://cmsdoc.cern.ch/~lpaciose/proxy", "proxy") 
         import requests
-        response = requests.get("https://cmsdoc.cern.ch/~lpaciose/proxy", verify=False)
+        response = requests.get("https://cmsdoc.cern.ch/~lpaciose/proxy", verify=True)
         with open("proxy", "wb") as f:
             f.write(response.content)
         
